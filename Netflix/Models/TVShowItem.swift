c3a650e29d84cb0cfd4b6cfc887696685d608991
Roierr <147010832+Roierr@users.noreply.github.com>
2025-06-18 19:09:09 -0600

    modelo series: TVShowItem.swift -Rogelio

---
+++ Netflix/Models/TVShowItem.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+struct SeriesItem: Identifiable, Codable {
+ let id: String
+ let title: String
+ let image: String
+ let description: String
+ let duration: String
+ let release: String
+ let rating: Double
+ let genre: String
+}
